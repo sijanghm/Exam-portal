7c01f505f0f4c1ae561c62c0e959b3a2df6a9043
sijanghm <sijan.ghimire29@gmail.com>
2021-12-16 13:19:01 +0545

    added crossorigin

--- exam-portal-backend/src/main/java/com/examportal/controller/UserController.java
+++ exam-portal-backend/src/main/java/com/examportal/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
